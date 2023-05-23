95b0bb7aa64fcb174b0ba4978c472ec85bfecc26
huykn15 <>
2023-05-22 19:37:45 -0700

    Phase 2

--- apps/stest.c
+++ apps/stest.c
@@ -14,4 +14,12 @@
 int main(){
     fs_mount("test.fs");
     fs_info();
+    fs_create("hi.txt");
+    fs_create("idk.txt");
+    fs_create("whatever.txt");
+    fs_ls();
+    fs_delete("whatever.txt");
+    fs_ls();
+    //fs_delete("hi.fs");
+    //fs_info();
 }
--- libfs/fs.c
+++ libfs/fs.c
@@ -4,11 +4,13 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "disk.h"
 #include "fs.h"
 
-#define MAX_FILE_COUNT 128
+#define EOC 0xFFFF
+
 /* TODO: Phase 1 */
 struct super_block{
 	uint8_t signature[8];
@@ -16,20 +18,25 @@ struct super_block{
 	int16_t root_index;
 	int16_t data_start_index;
 	int16_t data_block_count;
-	int8_t FAT_count;
+	int8_t fat_count;
 	int8_t padding[4079];
 };
 
 struct rdir_entry{
 	uint8_t file_name[16];
 	int32_t file_size;
-	int16_t data_index;
+	uint16_t data_index;
 	int8_t padding[10];
 };
 
-typedef int16_t FAT_entry;
+
+
+typedef int16_t fat_entry;
+
 
 struct super_block * cur_super_block;
+
+
 
 int fs_mount(const char *diskname)
 {
@@ -52,13 +59,13 @@ int fs_mount(const char *diskname)
 }
 
 int get_free_rdir_count(){
-	struct rdir_entry entries[MAX_FILE_COUNT];
+	struct rdir_entry entries[FS_FILE_MAX_COUNT];
 	int free_files = 0;
 	if(!cur_super_block){
 		return -1;
 	}
 	block_read(cur_super_block->root_index, &entries);
-	for (int i = 0; i < MAX_FILE_COUNT; i++){
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++){
 		if (entries[i].file_name[0] == 0){
 			free_files++;
 		}
@@ -66,13 +73,13 @@ int get_free_rdir_count(){
 	return free_files;
 }
 
-int get_free_FAT(){
+int get_free_fat(){
 	int free_entries = 0;
 	if(!cur_super_block){
 		return -1;
 	}
-	for(int i = 1; i <= cur_super_block->FAT_count; i++){
-		FAT_entry entries[BLOCK_SIZE / 2];
+	for(int i = 1; i <= cur_super_block->fat_count; i++){
+		fat_entry entries[BLOCK_SIZE / 2];
 		block_read(i, &entries);
 		for (int j = 0; j < BLOCK_SIZE / 2; j ++){
 			if (entries[j] == 0){
@@ -96,31 +103,132 @@ int fs_info(void)
 	}
 	printf("FS Info:\n");
 	printf("total_blk_count=%i\n", cur_super_block->block_count);
-	printf("fat_blk_count=%i\n", cur_super_block->FAT_count);
+	printf("fat_blk_count=%i\n", cur_super_block->fat_count);
 	printf("rdir_block=%i\n", cur_super_block->root_index);
 	printf("data_blk=%i\n", cur_super_block->data_start_index);
 	printf("data_blk_count=%i\n", cur_super_block->data_block_count);
-	printf("fat_free_ratio=%i/%i\n", get_free_FAT(), cur_super_block->FAT_count * 4096 / 2);
-	printf("rdir_free_ratio=%i/%i\n", get_free_rdir_count(), MAX_FILE_COUNT);
+	printf("fat_free_ratio=%i/%i\n", get_free_fat(), cur_super_block->fat_count * 4096 / 2);
+	printf("rdir_free_ratio=%i/%i\n", get_free_rdir_count(), FS_FILE_MAX_COUNT);
 	/* TODO: Phase 1 */
+	return 0;
+}
+
+int get_free_fat_index(){
+	if(!cur_super_block){
+		return -1;
+	}
+	for(int i = 1; i <= cur_super_block->fat_count; i++){
+		fat_entry entries[BLOCK_SIZE / 2];
+		block_read(i, &entries);
+		for (int j = 0; j < BLOCK_SIZE / 2; j ++){
+			if (entries[j] == 0){
+				return (BLOCK_SIZE / 2 * (i - 1) + j);
+			}
+		}
+	}
+	return -1;
+}
+
+int create_file_entry(struct rdir_entry * entries, const char * filename){
+	int i;
+	bool rdir_full = true;
+	if(!cur_super_block){
+		return -1;
+	}
+	for (i = 0; i < FS_FILE_MAX_COUNT; i++){
+		if (entries[i].file_name[0] == 0){
+			rdir_full = false;
+			break;
+		}
+	}
+	if(rdir_full){
+		return -1;
+	}
+	strcpy(entries[i].file_name, filename);
+	entries[i].data_index = EOC;
+	entries[i].file_size = 0;
+	return 0;
+}
+
+void get_fat_page(int16_t index, fat_entry * entries){
+	int i = 1;
+	while(index > BLOCK_SIZE / 2){
+		index -= BLOCK_SIZE / 2;
+	}
+	block_read(i, entries);
+}
+
+int clear_fat(uint16_t data_index){
+	while(data_index != EOC){
+		fat_entry entries[BLOCK_SIZE / 2];
+		fat_entry old_index = data_index;
+		get_fat_page(data_index, (fat_entry * )&entries);
+		data_index = data_index % (BLOCK_SIZE / 2);
+		entries[old_index] = 0;	
+	}
+	return 0;
+}
+
+int delete_rdir_entry(struct rdir_entry * entries, const char * filename){
+	int i;
+	bool missing = true;
+	for (i = 0; i < FS_FILE_MAX_COUNT; i++){
+		if (strncmp(entries[i].file_name, filename, strlen(filename))){
+			missing = false;
+			break;
+		}
+	}
+	if(missing){
+		return -1;
+	}
+	clear_fat(entries[i].data_index);
+	memset(&entries[i], 0, sizeof(struct rdir_entry));
 	return 0;
 }
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
+	struct rdir_entry entries[FS_FILE_MAX_COUNT];
+	block_read(cur_super_block->root_index, &entries);
+	if (!cur_super_block || strlen(filename) > FS_FILENAME_LEN){
+		return -1;
+	}
+	if(create_file_entry((struct rdir_entry *)&entries, filename)){
+		return -1;
+	}
+	block_write(cur_super_block->root_index, &entries);
 	return 0;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	if (!cur_super_block || strlen(filename) > FS_FILENAME_LEN){
+		return -1;
+	}
+
+	struct rdir_entry entries[FS_FILE_MAX_COUNT];
+	block_read(cur_super_block->root_index, &entries);
+	if(delete_rdir_entry((struct rdir_entry *)&entries, filename)){
+		return -1;
+	}
+	block_write(cur_super_block->root_index, entries);
 	return 0;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+	struct rdir_entry entries[FS_FILE_MAX_COUNT];
+	block_read(cur_super_block->root_index, &entries);
+	printf("FS ls:\n");
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++){
+		if (entries[i].file_name[0] != 0){
+			printf("file: %s, size: %i, data_blk: %u\n", entries[i].file_name, 
+				   entries[i].file_size, entries[i].data_index);
+		}
+	}
+
 	return 0;
 }
 
