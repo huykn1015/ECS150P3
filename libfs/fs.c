27d1dacae1e6e316741e41fcf250d4a1b15db706
huykn15 <>
2023-05-22 17:20:46 -0700

    Phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,6 +8,7 @@
 #include "disk.h"
 #include "fs.h"
 
+#define MAX_FILE_COUNT 128
 /* TODO: Phase 1 */
 struct super_block{
 	uint8_t signature[8];
@@ -16,12 +17,24 @@ struct super_block{
 	int16_t data_start_index;
 	int16_t data_block_count;
 	int8_t FAT_count;
+	int8_t padding[4079];
 };
 
+struct rdir_entry{
+	uint8_t file_name[16];
+	int32_t file_size;
+	int16_t data_index;
+	int8_t padding[10];
+};
+
+typedef int16_t FAT_entry;
+
 struct super_block * cur_super_block;
+
 int fs_mount(const char *diskname)
 {
 	if (block_disk_open(diskname)){
+		cur_super_block = NULL;
 		return -1;
 	}
 	cur_super_block = malloc(sizeof(uint8_t) * BLOCK_SIZE);
@@ -38,6 +51,38 @@ int fs_mount(const char *diskname)
 
 }
 
+int get_free_rdir_count(){
+	struct rdir_entry entries[MAX_FILE_COUNT];
+	int free_files = 0;
+	if(!cur_super_block){
+		return -1;
+	}
+	block_read(cur_super_block->root_index, &entries);
+	for (int i = 0; i < MAX_FILE_COUNT; i++){
+		if (entries[i].file_name[0] == 0){
+			free_files++;
+		}
+	}
+	return free_files;
+}
+
+int get_free_FAT(){
+	int free_entries = 0;
+	if(!cur_super_block){
+		return -1;
+	}
+	for(int i = 1; i <= cur_super_block->FAT_count; i++){
+		FAT_entry entries[BLOCK_SIZE / 2];
+		block_read(i, &entries);
+		for (int j = 0; j < BLOCK_SIZE / 2; j ++){
+			if (entries[j] == 0){
+				free_entries++;
+			}
+		}
+	}
+	return free_entries;
+}
+
 int fs_umount(void)
 {
 	return block_disk_close();
@@ -46,6 +91,17 @@ int fs_umount(void)
 
 int fs_info(void)
 {
+	if(!cur_super_block){
+		return -1;
+	}
+	printf("FS Info:\n");
+	printf("total_blk_count=%i\n", cur_super_block->block_count);
+	printf("fat_blk_count=%i\n", cur_super_block->FAT_count);
+	printf("rdir_block=%i\n", cur_super_block->root_index);
+	printf("data_blk=%i\n", cur_super_block->data_start_index);
+	printf("data_blk_count=%i\n", cur_super_block->data_block_count);
+	printf("fat_free_ratio=%i/%i\n", get_free_FAT(), cur_super_block->FAT_count * 4096 / 2);
+	printf("rdir_free_ratio=%i/%i\n", get_free_rdir_count(), MAX_FILE_COUNT);
 	/* TODO: Phase 1 */
 	return 0;
 }
